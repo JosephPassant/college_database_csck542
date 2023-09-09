9fdef60a470c20b1a314c84ce417faa98fa5b30f
Joseph Passant <j.passant@liverpool.ac.uk>
2023-09-10 00:19:20 +0100

    added calls to the AssignCourseToTeacher table to populate the course_teacher table

--- sql_db_creation_files/calls_to_stored_procedures.sql
+++ sql_db_creation_files/calls_to_stored_procedures.sql
@@ -10,3 +10,13 @@ CALL AssignCourseToSemester('jeffrey_abrams', 'password2', 4, 1);
 CALL AssignCourseToSemester('jeffrey_abrams', 'password2', 6, 2);
 CALL AssignCourseToSemester('jeffrey_abrams', 'password2', 8, 1);
 CALL AssignCourseToSemester('jeffrey_abrams', 'password2', 10, 2);
+
+
+-- Calls the AssignCourseToTeacher procedure in order to assign teachers to courses
+-- The course_semester table must be populated prior to calling this procedure
+-- This populates the course_teacher table
+CALL AssignCourseToTeacher('george_lucas', 'password1', 1, 1);
+CALL AssignCourseToTeacher('george_lucas', 'password1', 2, 2);
+CALL AssignCourseToTeacher('george_lucas', 'password1', 3, 4);
+CALL AssignCourseToTeacher('jeffrey_abrams', 'password2', 4, 6);
+CALL AssignCourseToTeacher('jeffrey_abrams', 'password2', 5, 9);
