36b26bf055deae9dc799f72470085d6623d09a62
Joseph Passant <j.passant@liverpool.ac.uk>
2023-09-10 00:11:40 +0100

    added AssignResultToStudent procedure

--- sql_db_creation_files/stored_procedures.sql
+++ sql_db_creation_files/stored_procedures.sql
@@ -117,6 +117,63 @@ BEGIN
     END IF;
 END;
 
+
+-- Create a stored procedure named 'AssignResultToStudent' that enables teachers of students on a course to assign
+-- a Pass or Fail result to a student enrolled in the course
+CREATE PROCEDURE AssignResultToStudent(
+    IN teacher_username VARCHAR(30),            -- Input for teacher username
+    IN teacher_password VARCHAR(255),           -- Input for teacher password
+    IN selected_student_id INT,                 -- Input for selected student ID
+    IN selected_course_semester_id INT,         -- Input for selected course-semester ID
+    IN given_result ENUM('Pass', 'Fail')        -- Input for the result to be assigned
+)
+BEGIN
+    -- Declare variables to hold counts and IDs
+    DECLARE selected_teacher_id INT DEFAULT 0;  -- Variable to hold the teacher ID
+    DECLARE teacher_count INT DEFAULT 0;        -- Variable to hold the count of matching teacher records
+    DECLARE teacher_course_count INT DEFAULT 0; -- Variable to hold the count of teacher-course assignments
+    DECLARE student_count INT DEFAULT 0;        -- Variable to hold the count of student-course enrollments
+
+    -- Validate teacher credentials by counting matching records in the 'teachers' table
+    SELECT COUNT(*) INTO teacher_count 
+    FROM teachers 
+    WHERE username = teacher_username AND user_password = teacher_password;
+
+    -- Get the teacher_id for the authenticated teacher
+    SELECT teacher_id INTO selected_teacher_id
+    FROM teachers
+    WHERE username = teacher_username AND user_password = teacher_password;
+
+    -- Check if the teacher is assigned to the course for the given semester
+    SELECT COUNT(*) INTO teacher_course_count
+    FROM course_teacher
+    WHERE teacher_id = selected_teacher_id AND course_semester_id = selected_course_semester_id;
+
+    -- Validate teacher credentials and course assignment
+    IF teacher_count = 0 THEN
+        SELECT 'Invalid teacher username or password' AS message;  -- Error message for invalid teacher credentials
+    ELSEIF teacher_course_count = 0 THEN
+        SELECT 'Teacher not assigned to the course for the given semester' AS message;  -- Error message for unassigned teacher
+    ELSE
+        -- Check if the student is enrolled in the course for the given semester
+        SELECT COUNT(*) INTO student_count
+        FROM course_student
+        WHERE student_id = selected_student_id AND course_semester_id = selected_course_semester_id;
+
+        -- Validate student enrollment
+        IF student_count = 0 THEN
+            SELECT 'Student not enrolled in the course for the given semester' AS message;  -- Error message for unenrolled student
+        ELSE
+            -- Assign the result to the student for the course in the given semester
+            UPDATE course_student
+            SET result = given_result
+            WHERE student_id = selected_student_id AND course_semester_id = selected_course_semester_id;
+
+            SELECT 'Result successfully assigned to student' AS message;  -- Success message
+        END IF;
+    END IF;
+END;
+
 //
 -- Reset the delimiter back to its default value
 DELIMITER ;
