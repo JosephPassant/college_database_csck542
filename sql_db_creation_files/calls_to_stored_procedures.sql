06f0f30c7a3af6a276bb9ffd8a2076fc1c27435a
Joseph Passant <j.passant@liverpool.ac.uk>
2023-09-10 00:31:05 +0100

    updated comments to show inputs required

--- sql_db_creation_files/calls_to_stored_procedures.sql
+++ sql_db_creation_files/calls_to_stored_procedures.sql
@@ -1,5 +1,6 @@
 -- Calls the AssignCourseToSemester procedure in order to assign courses to semesters
 -- populating the course_semester table
+-- Inputs required (admin_username VARCHAR(30), admin_password VARCHAR(255), selected_course_id INT, selected_semester_id INT)
 CALL AssignCourseToSemester('george_lucas', 'password1', 1, 1);
 CALL AssignCourseToSemester('george_lucas', 'password1', 3, 2);
 CALL AssignCourseToSemester('george_lucas', 'password1', 5, 1);
@@ -15,6 +16,7 @@ CALL AssignCourseToSemester('jeffrey_abrams', 'password2', 10, 2);
 -- Calls the AssignCourseToTeacher procedure in order to assign teachers to courses
 -- The course_semester table must be populated prior to calling this procedure
 -- This populates the course_teacher table
+-- Inputs required (admin_username VARCHAR(30), admin_password VARCHAR(255), selected_teacher_id INT, selected_course_semester_id INT)
 CALL AssignCourseToTeacher('george_lucas', 'password1', 1, 1);
 CALL AssignCourseToTeacher('george_lucas', 'password1', 2, 2);
 CALL AssignCourseToTeacher('george_lucas', 'password1', 3, 4);
@@ -25,6 +27,7 @@ CALL AssignCourseToTeacher('jeffrey_abrams', 'password2', 5, 9);
 -- Calls the AssignStudentToCourse procedure in order to enroll students on
 -- courses populating the course_student table
 -- The course_semester table must be populated prior to calling this procedure
+-- Input required (admin_password VARCHAR(255), selected_student_id INT, selected_course_semester_id INT)
 CALL AssignStudentToCourse('george_lucas', 'password1', 1, 1);
 CALL AssignStudentToCourse('jeffrey_abrams', 'password2', 2, 2);
 CALL AssignStudentToCourse('george_lucas', 'password1', 3, 4);
@@ -57,5 +60,6 @@ CALL AssignStudentToCourse('jeffrey_abrams', 'password2', 9, 9);
 
 -- Calls the AssignResultToCourse procedure in order to assign a Pass / Fail result to a student
 -- This updates the result collumn in the course_student table
+-- Input required (teacher_username VARCHAR(30), teacher_password VARCHAR(255), selected_student_id INT,
+-- selected_course_semester_id INT, given_result ENUM('Pass', 'Fail'))
 CALL AssignResultToStudent('master_yoda', 'password3', 1, 1, 'Pass');
-CALL AssignResultToStudent('master_yoda', 'password3', 1, 2, 'Pass');
