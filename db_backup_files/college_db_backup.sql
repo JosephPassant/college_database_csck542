df9c2652c0844edf70469717280a8c7e3b71ad17
Joseph Passant <j.passant@liverpool.ac.uk>
2023-09-10 16:23:12 +0100

    updated backup file

--- db_backup_files/college_db_backup.sql
+++ db_backup_files/college_db_backup.sql
@@ -59,7 +59,7 @@ CREATE TABLE `course_semester` (
   KEY `semester_id` (`semester_id`),
   CONSTRAINT `course_semester_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`),
   CONSTRAINT `course_semester_ibfk_2` FOREIGN KEY (`semester_id`) REFERENCES `semesters` (`semester_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,6 +68,7 @@ CREATE TABLE `course_semester` (
 
 LOCK TABLES `course_semester` WRITE;
 /*!40000 ALTER TABLE `course_semester` DISABLE KEYS */;
+INSERT INTO `course_semester` VALUES (1,1,1),(6,2,2),(2,3,2),(7,4,1),(3,5,1),(8,6,2),(4,7,2),(9,8,1),(5,9,1),(10,10,2);
 /*!40000 ALTER TABLE `course_semester` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,13 +83,13 @@ CREATE TABLE `course_student` (
   `course_student_id` int NOT NULL AUTO_INCREMENT,
   `student_id` int DEFAULT NULL,
   `course_semester_id` int DEFAULT NULL,
-  `result` enum('pass','fail') DEFAULT NULL,
+  `result` enum('Pass','Fail') DEFAULT NULL,
   PRIMARY KEY (`course_student_id`),
   UNIQUE KEY `student_id` (`student_id`,`course_semester_id`),
   KEY `course_semester_id` (`course_semester_id`),
   CONSTRAINT `course_student_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`),
   CONSTRAINT `course_student_ibfk_2` FOREIGN KEY (`course_semester_id`) REFERENCES `course_semester` (`course_semester_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,6 +98,7 @@ CREATE TABLE `course_student` (
 
 LOCK TABLES `course_student` WRITE;
 /*!40000 ALTER TABLE `course_student` DISABLE KEYS */;
+INSERT INTO `course_student` VALUES (1,1,1,'Pass'),(2,2,2,'Fail'),(3,3,4,'Pass'),(4,4,6,'Pass'),(5,5,9,'Pass'),(6,6,1,NULL),(7,7,2,NULL),(8,8,4,NULL),(9,9,6,NULL),(10,10,9,NULL),(11,11,1,NULL),(12,12,2,NULL),(13,13,4,NULL),(14,14,6,NULL),(15,15,9,NULL),(16,16,9,NULL),(17,17,1,NULL),(18,18,2,NULL),(19,19,4,NULL),(20,20,6,NULL),(21,20,1,'Pass');
 /*!40000 ALTER TABLE `course_student` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -116,7 +118,7 @@ CREATE TABLE `course_teacher` (
   KEY `course_semester_id` (`course_semester_id`),
   CONSTRAINT `course_teacher_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`teacher_id`),
   CONSTRAINT `course_teacher_ibfk_2` FOREIGN KEY (`course_semester_id`) REFERENCES `course_semester` (`course_semester_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,6 +127,7 @@ CREATE TABLE `course_teacher` (
 
 LOCK TABLES `course_teacher` WRITE;
 /*!40000 ALTER TABLE `course_teacher` DISABLE KEYS */;
+INSERT INTO `course_teacher` VALUES (1,1,1),(2,2,2),(3,3,4),(4,4,6),(5,5,9);
 /*!40000 ALTER TABLE `course_teacher` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -151,6 +154,21 @@ LOCK TABLES `courses` WRITE;
 INSERT INTO `courses` VALUES (1,'Jedi 101'),(2,'The Force'),(3,'Ethics'),(4,'History'),(5,'Diplomacy'),(6,'Leadership'),(7,'Philosophy'),(8,'Medicine'),(9,'Combat'),(10,'Politics');
 /*!40000 ALTER TABLE `courses` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `coursesofferedinsemester`
+--
+
+DROP TABLE IF EXISTS `coursesofferedinsemester`;
+/*!50001 DROP VIEW IF EXISTS `coursesofferedinsemester`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `coursesofferedinsemester` AS SELECT 
+ 1 AS `course_name`,
+ 1 AS `semester_name`,
+ 1 AS `start_date`,
+ 1 AS `end_date`*/;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `semesters`
@@ -233,6 +251,24 @@ LOCK TABLES `teachers` WRITE;
 INSERT INTO `teachers` VALUES (1,'Yoda','Master','master_yoda','password3'),(2,'Mace','Windu','mace_windu','password4'),(3,'Obi-Wan','Kenobi','obi-wan_kenobi','password5'),(4,'Ki-Adi','Mundi','ki-adi_mundi','password6'),(5,'Plo','Koon','plo_koon','password7'),(6,'Shaak','Ti','shaak_ti','password8'),(7,'Kit','Fisto','kit_fisto','password9');
 /*!40000 ALTER TABLE `teachers` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Final view structure for view `coursesofferedinsemester`
+--
+
+/*!50001 DROP VIEW IF EXISTS `coursesofferedinsemester`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `coursesofferedinsemester` AS select `c`.`course_name` AS `course_name`,`s`.`semester_name` AS `semester_name`,`s`.`start_date` AS `start_date`,`s`.`end_date` AS `end_date` from ((`course_semester` `cs` join `courses` `c` on((`cs`.`course_id` = `c`.`course_id`))) join `semesters` `s` on((`cs`.`semester_id` = `s`.`semester_id`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -243,4 +279,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-10 15:16:06
+-- Dump completed on 2023-09-10 16:21:54
